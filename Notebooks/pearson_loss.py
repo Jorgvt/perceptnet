e697c28ae598457af1a6b4459601d30c54cada69
Jorgvt <vila_tronik@hotmail.com>
2022-04-13 14:33:40 +0200

    Squeeze the inputs first to avoid dimension problems.

--- Notebooks/pearson_loss.py
+++ Notebooks/pearson_loss.py
@@ -7,8 +7,10 @@ class PearsonCorrelation(tf.keras.losses.Loss):
     """
 
     def call(self, y_true, y_pred):
-        y_true_mean = tf.reduce_mean(y_true, axis=0)
-        y_pred_mean = tf.reduce_mean(y_pred, axis=0)
+        y_true = tf.squeeze(y_true)
+        y_pred = tf.squeeze(y_pred)
+        y_true_mean = tf.reduce_mean(y_true)
+        y_pred_mean = tf.reduce_mean(y_pred)
         num = y_true-y_true_mean
         num *= y_pred-y_pred_mean
         num = tf.reduce_sum(num)
