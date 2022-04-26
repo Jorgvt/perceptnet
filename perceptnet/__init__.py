044acd4372f4adf58e32033bebcae4e5620380ae
Jorgvt <vila_tronik@hotmail.com>
2022-04-26 21:06:45 +0200

    Cleaned __init__.py. #3

--- perceptnet/__init__.py
+++ perceptnet/__init__.py
@@ -1,15 +1,3 @@
-# from .test import *
-# from .pearson_loss import *
-
-# __all__ = ['test']
-
-# import perceptnet.test
-# import perceptnet.test
 import perceptnet.networks
 import perceptnet.callbacks
-# from perceptnet.GDN_Jorge import GDN
 from perceptnet.pearson_loss import PearsonCorrelation
-# from perceptnet.networks import PerceptNet, PerceptNetSeq, PerceptNetRegressor, PerceptNetRegressorFine
-# import perceptnet.pearson_loss
-
-# __all__ = ['PerceptNet']
