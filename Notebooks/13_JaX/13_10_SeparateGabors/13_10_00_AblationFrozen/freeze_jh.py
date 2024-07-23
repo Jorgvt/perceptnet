2b55458a54181575706d5c02bc1c8ee503048f86
Jorgvt <vila_tronik@hotmail.com>
2024-07-23 16:38:23 +0200

    :sparkles: config files

---
+++ Notebooks/13_JaX/13_10_SeparateGabors/13_10_00_AblationFrozen/freeze_jh.py
@@ -0,0 +1,33 @@
+from ml_collections import ConfigDict
+def get_config():
+    config = {
+        "BATCH_SIZE": 64,
+        "EPOCHS": 500,
+        "LEARNING_RATE": 3e-3,
+        "INITIAL_LR": 1e-2,
+        "PEAK_LR": 4e-2,
+        "END_LR": 5e-3,
+        "WARMUP_EPOCHS": 15,
+        "SEED": 42,
+        "GDN_CLIPPING": True,
+        "NORMALIZE_PROB": False,
+        "NORMALIZE_ENERGY": True,
+        "ZERO_MEAN": True,
+        "USE_BIAS": False,
+        "CS_KERNEL_SIZE": 21,
+        "GDNGAUSSIAN_KERNEL_SIZE": 11,
+        "GABOR_KERNEL_SIZE": 31,
+        "N_SCALES": 4,
+        "N_ORIENTATIONS": 16,
+        # "N_GABORS": 128,
+        "USE_GAMMA": True,
+        "INIT_JH": True,
+        "INIT_GABOR": True,
+        "TRAIN_JH": False,
+        "TRAIN_CS": True,
+        "TRAIN_GABOR": True,
+        "A_GABOR": True,
+        "A_GDNSPATIOFREQORIENT": True,
+        "TRAIN_ONLY_LAST_GDN": False,
+    }
+    return ConfigDict(config)
