68665f3ef88c4158bc2149070cd851115e05d594
Jorgvt <vila_tronik@hotmail.com>
2024-10-14 17:20:45 +0200

    initialization script

---
+++ Notebooks/13_JaX/13_10_SeparateGabors/13_10_04_Receptive_Fields/obtain_receptive_fields_init.sh
@@ -0,0 +1,7 @@
+python -u 13_10_04_00_Receptive_Field.py -p Receptive_Fields_Init/GDNGamma -l GDNGamma_0 --id 2ploco2u --init
+python -u 13_10_04_00_Receptive_Field.py -p Receptive_Fields_Init/Color -l Color --id 2ploco2u --init
+python -u 13_10_04_00_Receptive_Field.py -p Receptive_Fields_Init/GDNColor -l GDN_0 --id 2ploco2u --init
+python -u 13_10_04_00_Receptive_Field.py -p Receptive_Fields_Init/CenterSurround -l CenterSurroundLogSigmaK_0 --id 2ploco2u --init
+python -u 13_10_04_00_Receptive_Field.py -p Receptive_Fields_Init/GDNGaussian -l GDNGaussian_0 --id 2ploco2u --init
+python -u 13_10_04_00_Receptive_Field.py -p Receptive_Fields_Init/Gabor -l GaborLayerGammaHumanLike__0 --id 2ploco2u --init
+python -u 13_10_04_00_Receptive_Field.py -p Receptive_Fields_Init/GDNFinal -l GDNSpatioChromaFreqOrient_0 --id 2ploco2u --init
