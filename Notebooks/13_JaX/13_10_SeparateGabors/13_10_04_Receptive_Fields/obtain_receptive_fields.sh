4152b3a65b4e3bcf40fe9632d11c8612665442d8
Jorgvt <vila_tronik@hotmail.com>
2024-10-14 16:42:51 +0200

    fixed layer names

--- Notebooks/13_JaX/13_10_SeparateGabors/13_10_04_Receptive_Fields/obtain_receptive_fields.sh
+++ Notebooks/13_JaX/13_10_SeparateGabors/13_10_04_Receptive_Fields/obtain_receptive_fields.sh
@@ -1,7 +1,7 @@
 python -u 13_10_04_00_Receptive_Field.py -p Receptive_Fields/GDNGamma -l GDNGamma_0 --id 2ploco2u
-# python -u 13_10_04_00_Receptive_Field.py -p Receptive_Fields/Color -l Color --id 2ploco2u
-# python -u 13_10_04_00_Receptive_Field.py -p Receptive_Fields/GDNColor -l GDN_0 --id 2ploco2u
-# python -u 13_10_04_00_Receptive_Field.py -p Receptive_Fields/CenterSurround -l CenterSurroundLogSigmaK_0 --id 2ploco2u
-# python -u 13_10_04_00_Receptive_Field.py -p Receptive_Fields/GDNGaussian -l GDNGaussian_0 --id 2ploco2u
-# python -u 13_10_04_00_Receptive_Field.py -p Receptive_Fields/Gabor -l GaborLayerGammaHumanLike__0 --id 2ploco2u
-# python -u 13_10_04_00_Receptive_Field.py -p Receptive_Fields/GDNFinal -l GDNSpatioChromaFreqOrient_0 --id 2ploco2u
+python -u 13_10_04_00_Receptive_Field.py -p Receptive_Fields/Color -l Color --id 2ploco2u
+python -u 13_10_04_00_Receptive_Field.py -p Receptive_Fields/GDNColor -l GDN_0 --id 2ploco2u
+python -u 13_10_04_00_Receptive_Field.py -p Receptive_Fields/CenterSurround -l CenterSurroundLogSigmaK_0 --id 2ploco2u
+python -u 13_10_04_00_Receptive_Field.py -p Receptive_Fields/GDNGaussian -l GDNGaussian_0 --id 2ploco2u
+python -u 13_10_04_00_Receptive_Field.py -p Receptive_Fields/Gabor -l GaborLayerGammaHumanLike__0 --id 2ploco2u
+python -u 13_10_04_00_Receptive_Field.py -p Receptive_Fields/GDNFinal -l GDNSpatioChromaFreqOrient_0 --id 2ploco2u
