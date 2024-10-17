8acea126d706836626259e685aa93478c65f82c1
Jorgvt <vila_tronik@hotmail.com>
2024-10-17 13:03:28 +0200

    :bug: typo in script name

--- Notebooks/13_JaX/13_10_SeparateGabors/13_10_04_Receptive_Fields/obtain_receptive_fields_noparam.sh
+++ Notebooks/13_JaX/13_10_SeparateGabors/13_10_04_Receptive_Fields/obtain_receptive_fields_noparam.sh
@@ -1,7 +1,7 @@
-python -u 13_10_04_00_Receptive_Field_NoParam.py -p Receptive_Fields_NoParam/GDNGamma -l GDN_0 --id rlmp4ntk
-python -u 13_10_04_00_Receptive_Field_NoParam.py -p Receptive_Fields_NoParam/Color -l Color --id rlmp4ntk
-python -u 13_10_04_00_Receptive_Field_NoParam.py -p Receptive_Fields_NoParam/GDNColor -l GDN_1 --id rlmp4ntk
-python -u 13_10_04_00_Receptive_Field_NoParam.py -p Receptive_Fields_NoParam/CenterSurround -l Conv_0 --id rlmp4ntk
-python -u 13_10_04_00_Receptive_Field_NoParam.py -p Receptive_Fields_NoParam/GDNGaussian -l GDN_2 --id rlmp4ntk
-python -u 13_10_04_00_Receptive_Field_NoParam.py -p Receptive_Fields_NoParam/Gabor -l Conv_1 --id rlmp4ntk
-python -u 13_10_04_00_Receptive_Field_NoParam.py -p Receptive_Fields_NoParam/GDNFinal -l GDN_3 --id rlmp4ntk
+python -u 13_10_04_01_Receptive_Field_NoParam.py -p Receptive_Fields_NoParam/GDNGamma -l GDN_0 --id rlmp4ntk --testing
+# python -u 13_10_04_01_Receptive_Field_NoParam.py -p Receptive_Fields_NoParam/Color -l Color --id rlmp4ntk
+# python -u 13_10_04_01_Receptive_Field_NoParam.py -p Receptive_Fields_NoParam/GDNColor -l GDN_1 --id rlmp4ntk
+# python -u 13_10_04_01_Receptive_Field_NoParam.py -p Receptive_Fields_NoParam/CenterSurround -l Conv_0 --id rlmp4ntk
+# python -u 13_10_04_01_Receptive_Field_NoParam.py -p Receptive_Fields_NoParam/GDNGaussian -l GDN_2 --id rlmp4ntk
+# python -u 13_10_04_01_Receptive_Field_NoParam.py -p Receptive_Fields_NoParam/Gabor -l Conv_1 --id rlmp4ntk
+# python -u 13_10_04_01_Receptive_Field_NoParam.py -p Receptive_Fields_NoParam/GDNFinal -l GDN_3 --id rlmp4ntk
